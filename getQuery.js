f450b18d84cbe9674552bcaca251af02f7ec7c56
Jerome PL <jerome-pl2@hotmail.ca>
2016-02-20 16:51:16 -0500

    Add method to get the jquery query from a DOM element

---
+++ getQuery.js
@@ -0,0 +1,38 @@
+//From an DOM element, get a query to that DOM element
+function getQuery(e) {
+    var element = e;
+    var query = [];
+
+    while(!element.id && element != $('html')[0]) {
+        var jEl = $(element);
+        if(jEl.index() > 0) { //Since nth-child, eq and nth-of-type dont work well with multiple sub-children, give up and just store the whole element
+            query = [e];
+            break;
+        }
+        else {
+            query.push(element.localName);
+        }
+        element = $(element).parent()[0];
+    }
+
+    var id;
+    if(id = element.id) //Add the last id
+        query.push('#' + id);
+
+    //Reverse the array
+    for(var i = 0; i < query.length / 2; i++) {
+        var temp = query[i];
+        query[i] = query[query.length - 1 - i];
+        query[query.length - 1 - i] = temp;
+    }
+
+    //Assemble the string
+    var finalString = "";
+    if(query.length > 1)
+        finalString = query.join(" ");
+    else
+        finalString = query[0];
+
+    console.log(finalString);
+    console.log($(finalString)[0]);
+}
